ae05cb310bee4e2f431b11864b01165d0220c8f9
Sylvain062 <151930447+Sylvain062@users.noreply.github.com>
2024-05-10 16:56:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ if page == pages[3] :
   <li>Élément 2</li>
   <li>Élément 3</li>
 </ul>
-""")
+""", unsafe_allow_html=True)
     st.write("### Introduction :")
     st.write("Pour réaliser les étapes du preprocessing nous avons utilisé les procédures habituelles telles que nous les avons apprises en formation à savoir :")
     st.write("‣ Onehotencoding des genres")
