235ab4b8bbde16ca0c2914694093738e5f82a1e0
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-10 15:18:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,12 +66,12 @@ if page == pages[2] :
     top_pays = pays_counts[:8]
     autres = pays_counts[8:].sum()
     top_pays['Autres'] = autres
-    plt.figure(figsize=(10, 8))
+    fig = plt.figure(figsize=(10, 8))
     wedges, texts, autotexts = plt.pie(top_pays, autopct='%1.1f%%', startangle=140, pctdistance=0.9)
     plt.legend(wedges, top_pays.index, title="Pays", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
     plt.title('Répartition des films par pays')
     plt.axis('equal')
-    plt.show()
+    st.pyplot(fig)
 
     
 
