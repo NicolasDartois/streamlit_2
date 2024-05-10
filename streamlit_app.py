36f5d9c26723c2c68669d3538e1b9a9fdd0d7b3f
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-10 17:01:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,9 @@ if page == pages[2] :
 
 if page == pages[3] :
     st.write("### Introduction :")
+    st.write("Exemple de texte")
     st.write("### Traitement des valeurs manquantes :")
+    st.write("Exemple de texte")
     st.write("### Formatage des données :")
     st.write("Pour réaliser les étapes du preprocessing nous avons utilisé les procédures habituelles telles que nous les avons apprises en formation à savoir :")
     st.markdown("""
