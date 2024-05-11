0a1c70fd3645b40ddf8f3693237a010632ce2d41
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-11 14:45:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,6 +96,40 @@ if page == pages[2] :
     st.plotly_chart(fig3)
 
     #---------------#
+    correlation = allocine['cumul_france'].corr(allocine['premiere_semaine_france'])
+    fig4 = px.scatter(
+    allocine, 
+    x='cumul_france', 
+    y='premiere_semaine_france',
+    hover_data=['titre_original'],
+    title=f'Corrélation entre le cumul en France et la première semaine en France: {correlation:.2f}',
+    labels={'cumul_france': 'Cumul en France', 'premiere_semaine_france': 'Première semaine en France'},
+    opacity=0.5,
+    trendline='ols'
+    )
+
+    fig4.update_layout(margin={'l': 40, 'b': 40, 't': 80, 'r': 40}, hovermode='closest')
+    fig4.update_xaxes(showgrid=True, title='Cumul en France')
+    fig4.update_yaxes(showgrid=True, title='Première semaine en France')
+    st.plotly_chart(fig4)
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
+
+    #---------------#
     
 #########################################################
 if page == pages[3] :
