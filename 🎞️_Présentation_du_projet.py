bd7f9314a3f0fc66cbee8cd1c32103692ac1f5c9
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 13:47:22 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Projet Ciné", layout="wide") 
+st.set_page_config(page_title="Projet Ciné", layout="centered") 
 
 st.title("L'IA au service de la production cinématographique !")
+
+with open('include/carousel.html', 'r', encoding='utf-8') as f:
+    carousel_html = f.read()
+if page == pages[0] : 
+    components.html(carousel_html, height=600, width=800)
