dc7c242023dc4f2cd8d3ea74d182e5a9d09931ca
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 13:09:08 +0200

    Add files via upload

---
+++ pages/4_Préparation_les_données_-_Preprocessing.py
@@ -0,0 +1,8 @@
+import streamlit.components.v1 as components
+import streamlit as st
+
+with open('include/carousel.html', 'r', encoding='utf-8') as f:
+    carousel_html = f.read()
+
+def page1():
+    components.html(carousel_html, height=600, width=800)
