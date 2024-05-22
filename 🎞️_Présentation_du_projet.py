373ed7977f7801be2f2738d98002a5bed46a8a4a
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 13:48:59 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Projet Ciné", layout="centered") 
+st.set_page_config(page_title="Projet Ciné", layout="wide") 
 
 st.title("L'IA au service de la production cinématographique !")
 
