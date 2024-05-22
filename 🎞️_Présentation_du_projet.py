8465e449850c80bf70c64608ad4f45ea81aa18a1
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 16:16:32 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -18,8 +18,6 @@ st.markdown(
     <style>
     .box {
         background-color: white;
-        padding: 20px;
-        margin: 20px;
         box-shadow: 0 10px 12px rgba(0, 0, 0, 0.1);
         border-radius: 10px;
         text-align: left;
