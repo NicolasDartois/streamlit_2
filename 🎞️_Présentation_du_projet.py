d64c236d4031bde00b4af50583200aaa03265135
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 16:26:06 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -6,7 +6,7 @@ st.title("🎥 L'IA au service de la production cinématographique ! 🎥")
 background_image = '''
     <style>
     body {
-        background-image: url("https://github.com/NicolasDartois/streamlit_2/blob/main/images/background.jpg");
+        background-image: url("https://github.com/NicolasDartois/streamlit_2/blob/main/images/background.jpg?raw=true");
         background-size: cover;
     }
     .box {
