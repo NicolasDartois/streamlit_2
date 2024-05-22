ba5ec48317070438b31219c9d21cbc459a11a111
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 17:29:35 +0200

    Update 3_📊_Analyse_des_Données_(DataViz).py

--- pages/3_📊_Analyse_des_Données_(DataViz).py
+++ pages/3_📊_Analyse_des_Données_(DataViz).py
@@ -11,6 +11,29 @@ from plotly.subplots import make_subplots
 from bokeh.plotting import figure
 from bokeh.models import HoverTool
 
+background_image = '''
+    <style>
+    .stApp {
+        background-color: white;
+        background-image: url("https://github.com/NicolasDartois/streamlit_2/blob/main/images/background.jpg?raw=true");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-position: center;
+    }
+    .box {
+        background-color: white;
+        padding: 20px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        box-shadow: 0 10px 12px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+        text-align: left;
+    }
+    </style>
+    '''
+st.markdown(background_image, unsafe_allow_html=True)
+
 allocine = pd.read_csv('data/allocine.csv')
 
 
@@ -31,6 +54,7 @@ fig1.update_layout(
             legend_title="Pays"
             )
 st.plotly_chart(fig1)
+
 st.markdown('</p></div>', unsafe_allow_html=True)
 #---------------#
 
