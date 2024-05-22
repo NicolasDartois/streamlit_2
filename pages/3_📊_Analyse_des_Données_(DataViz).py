0974a0933d98c5bf575f362c9b08c1619ce1fa01
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 20:31:56 +0200

    Update 3_📊_Analyse_des_Données_(DataViz).py

--- pages/3_📊_Analyse_des_Données_(DataViz).py
+++ pages/3_📊_Analyse_des_Données_(DataViz).py
@@ -31,8 +31,7 @@ background_image = '''
         margin-bottom: 20px;
         box-shadow: 0 10px 12px rgba(0, 0, 0, 0.1);
         border-radius: 10px;
-        text-align: left;
-        align="justify";
+        text-align: justify;
     }
     </style>
     '''
