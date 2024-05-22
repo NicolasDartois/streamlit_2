e1b3160ebbb2a33e4b48ca083ff7834d8963fb15
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 13:33:41 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -1,30 +1,4 @@
 import streamlit as st
-import os
-import importlib.util
-
-def load_module(module_name, file_path):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
-
-pages_dir = os.path.join(os.path.dirname(__file__), 'pages')
-
-pages={
-    "page1" : "Présentation du projet",
-    "page2" : "Collecte et Exploration des Données",
-    "page3" : "Analyse des Données (DataViz)",
-    "page4" : "Préparation les données - Preprocessing",
-    "page5" : "Présentation du modèle",
-    "page6" : "DEMONSTRATION"
-    }
-
-page_modules = {}
-for i, j in pages.items():
-    module = load_module(i, os.path.join(pages_dir, f'{i}.py'))
-    page_modules[j] = module
-    st.write(page_modules)
-
 
 st.set_page_config(page_title="Projet Ciné", layout="centered") 
 
