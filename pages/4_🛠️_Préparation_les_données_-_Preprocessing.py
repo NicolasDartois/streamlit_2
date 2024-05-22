1b463e6ffde62dee0787101cb11230fcf02e05db
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 13:39:19 +0200

    Update 4_🛠️_Préparation_les_données_-_Preprocessing.py

--- pages/4_🛠️_Préparation_les_données_-_Preprocessing.py
+++ pages/4_🛠️_Préparation_les_données_-_Preprocessing.py
@@ -1,8 +1,28 @@
-import streamlit.components.v1 as components
 import streamlit as st
 
-with open('include/carousel.html', 'r', encoding='utf-8') as f:
-    carousel_html = f.read()
-
-def page1():
-    components.html(carousel_html, height=600, width=800)
+st.write("### Introduction :")
+st.write("Le prétraitement (preprocessing) des données vise à préparer les données brutes pour que notre modèle futur soit le plus performant possible  :")
+st.markdown("""
+<ul>
+    <li>Nettoyant les données en éliminant les valeurs aberrantes (réalisé juste avant la modélisation), et en gérant les valeurs manquantes. NB : le dédoublonnage n'était pas nécessaire.</li>
+    <li>Transformant les features en formats compatibles avec les algorithmes d'apprentissage automatique, tels que la normalisation des valeurs et les onehotencoding.</li>
+    <li>En améliorant la qualité des données en calculant des scores afin d'optimiser les performances des modèles et de garantir des résultats plus fiables.</li>
+</ul>
+""", unsafe_allow_html=True)
+st.write("### Traitement des valeurs manquantes :")
+st.write("Exemple de texte")
+st.write("### Formatage des données :")
+st.write("Les étapes de formatage des données :")
+st.markdown("""
+<ul>
+    <li>Onehotencoding des genres</li>
+    <li>Onehotencoding de l'origine du pays du film (en ayant retenu que 3 catégories : France ou USA ou autre (si France et USA sont en False)</li>
+    <li>conversion et reformatage des données budgets : identification de toutes les devises différentes, conversion en euros en utilisant le taux de change à date de chaque devise</li>
+    <li>extraction des jours, jours_semaine, mois de la date de sortie des films, puis standardisation cyclique des variables obtenues</li>
+    <li>Mise en oeuvre d'un score acteurs, d'un score réalisateurs, d'un score scenaristes, d'un score distributeur</li>
+    <li>Description schématique du score Acteurs :</li>
+</ul>
+<br>
+<br>
+""", unsafe_allow_html=True)
+st.image('images/score_acteur.png')
