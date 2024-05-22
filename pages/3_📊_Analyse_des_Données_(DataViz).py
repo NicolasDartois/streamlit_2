e98ac35a7200324359e39d328ca44781bfb45721
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 13:09:45 +0200

    Update and rename 3_Analyse_des_Données_(DataViz).py to 3_📊_Analyse_des_Données_(DataViz).py

--- pages/3_📊_Analyse_des_Données_(DataViz).py
+++ pages/3_📊_Analyse_des_Données_(DataViz).py
@@ -3,6 +3,6 @@ import streamlit as st
 
 with open('include/carousel.html', 'r', encoding='utf-8') as f:
     carousel_html = f.read()
-
+    
 def page1():
     components.html(carousel_html, height=600, width=800)
