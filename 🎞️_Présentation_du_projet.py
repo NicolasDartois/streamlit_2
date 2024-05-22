49983957793abeba65e809647fe5defae31df8a5
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-22 16:18:02 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -20,6 +20,7 @@ st.markdown(
         background-color: white;
         padding: 20px;
         margin-top: 20px;
+        margin-bottom: 20px;
         box-shadow: 0 10px 12px rgba(0, 0, 0, 0.1);
         border-radius: 10px;
         text-align: left;
