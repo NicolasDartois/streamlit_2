eddab639c131e451543763f3900fa553fe4de89d
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 11:56:02 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -15,12 +15,18 @@ df_scenar = pd.read_csv('data/score_scenar.csv')
 
 acteur1 = st.selectbox('Choisir l\'acteur principal :', df_acteur['Acteur'])
 score_acteur1 = df_acteur[df_acteur['Acteur'] == acteur1]['Score'].values[0]
-st.write(score_acteur1)
 
 acteur2 = st.selectbox('Choisir l\'acteur secondaire :', df_acteur['Acteur'])
 score_acteur2 = df_acteur[df_acteur['Acteur'] == acteur2]['Score'].values[0]
-st.write(score_acteur2)
-st.write((score_acteur1*1.125)+(score_acteur2*0.75))
+
+score_acteur = (score_acteur1*1.125)+(score_acteur2*0.75)
+
+real = st.selectbox('Choisir le réalisateur :', df_real['realisateur'])
+score_real = df_real[df_real['realisateur'] == real]['score'].values[0]
+
+st.write(score_acteur)
+st.write(score_real)
+
 
 
 
