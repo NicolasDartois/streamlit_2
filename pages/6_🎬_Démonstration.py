1355aa61685ae441e6a02ea419defebf0686f320
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 16:18:32 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -16,7 +16,7 @@ df_real = pd.read_csv('data/score_real.csv')
 df_scenar = pd.read_csv('data/score_scenar.csv')
 df_distrib = pd.read_csv('data/score_distrib.csv')
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 with col1:
     acteur1 = st.selectbox('Choisir l\'acteur principal :', df_acteur['Acteur'])
     score_acteur1 = df_acteur[df_acteur['Acteur'] == acteur1]['Score'].values[0]
@@ -24,14 +24,16 @@ with col2:
     acteur2 = st.selectbox('Choisir l\'acteur secondaire :', df_acteur['Acteur'])
     score_acteur2 = df_acteur[df_acteur['Acteur'] == acteur2]['Score'].values[0]
     score_acteur = (score_acteur1*1.125)+(score_acteur2*0.75)
-with col3:    
+    
+col1, col2 = st.columns(2)    
+with col1:    
     real = st.selectbox('Choisir le réalisateur :', df_real['realisateur'])
     score_real = df_real[df_real['realisateur'] == real]['score'].values[0]
-
-col1, col2, col3 = st.columns(3)
-with col1:
+with col2:
     scenar = st.selectbox('Choisir le scénariste :', df_scenar['scenariste'])
     score_scenar = df_scenar[df_scenar['scenariste'] == scenar]['score'].values[0]
+
+col1, col2 = st.columns(2)
 with col2:    
     distrib = st.selectbox('Choisir le distributeur :', df_distrib['distributeur'])
     score_distrib  = df_distrib[df_distrib['distributeur'] == distrib]['score'].values[0]
