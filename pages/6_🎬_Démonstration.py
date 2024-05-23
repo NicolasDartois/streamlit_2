511527e0f9536c510eeab139d7914d786c56b10a
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 13:54:55 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -13,6 +13,7 @@ model = jl.load("models/GB.joblib")
 df_acteur = pd.read_csv('data/score_acteur.csv')
 df_real = pd.read_csv('data/score_real.csv')
 df_scenar = pd.read_csv('data/score_scenar.csv')
+df_distrib = pd.read_csv('data/score_distrib.csv')
 
 acteur1 = st.selectbox('Choisir l\'acteur principal :', df_acteur['Acteur'])
 score_acteur1 = df_acteur[df_acteur['Acteur'] == acteur1]['Score'].values[0]
@@ -27,6 +28,9 @@ score_real = df_real[df_real['realisateur'] == real]['score'].values[0]
 
 scenar = st.selectbox('Choisir le scénariste :', df_scenar['scenariste'])
 score_scenar = df_scenar[df_scenar['scenariste'] == scenar]['score'].values[0]
+
+distrib = st.selectbox('Choisir le distributeur :', df_distrib['distributeur'])
+score_scenar = df_distrib[df_distrib['distributeur'] == distrib]['score'].values[0]
 
 date_sortie = st.date_input('Choisir la date de sortie', min_value=date(2000, 1, 1), max_value=date(2023, 12, 31))
 
