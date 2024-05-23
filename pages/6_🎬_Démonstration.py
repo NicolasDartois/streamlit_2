4b6d1b0ea75144e9ee40af33261a589883b58044
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 11:48:14 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -7,7 +7,36 @@ css_and_credit()
 
 st.header("🎬Démonstration🎬")
 
-clf = jl.load("models/GB.joblib")
+model = jl.load("models/GB.joblib")
+
+df_acteur = pd.read_csv('data/score_acteur.csv')
+df_real = pd.read_csv('data/score_real.csv')
+df_scenar = pd.read_csv('data/score_scenar.csv')
+
+acteur = st.selectbox('Choisir un acteur', df_acteur['acteur'])
+score_acteur = df_acteur[df_acteur['acteur'] == acteur]['score_acteur'].values[0]
+
+
+input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+if st.button('Prédire'):
+    prediction = model.predict(input_data)
+    st.subheader(f'La classe prédite est : {prediction[0]}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 df = pd.read_csv('data/score_acteur.csv')
 st.dataframe(df)
 
