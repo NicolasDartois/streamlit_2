2e8b108fc509abb2271ebb4666fd09ce5c8c5688
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-24 00:14:25 +0200

    Update css_and_credit.py

--- include/css_and_credit.py
+++ include/css_and_credit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def css_and_credit():
-    st.sidebar.image("images/img_sidebar.png", use_column_width=True)
+    #st.sidebar.image("images/img_sidebar.png", use_column_width=True)
     for i in range(2):
         st.sidebar.markdown("<br>", unsafe_allow_html=True)
     st.sidebar.subheader("🎥oct23_cda_exploitation-cinématographique🎥")
