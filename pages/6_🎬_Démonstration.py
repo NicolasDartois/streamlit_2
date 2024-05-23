0b3476cbedf1eb2da5138e51fed8140c87a9793b
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 16:17:31 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -16,32 +16,39 @@ df_real = pd.read_csv('data/score_real.csv')
 df_scenar = pd.read_csv('data/score_scenar.csv')
 df_distrib = pd.read_csv('data/score_distrib.csv')
 
-acteur1 = st.selectbox('Choisir l\'acteur principal :', df_acteur['Acteur'])
-score_acteur1 = df_acteur[df_acteur['Acteur'] == acteur1]['Score'].values[0]
+col1, col2, col3 = st.columns(3)
+with col1:
+    acteur1 = st.selectbox('Choisir l\'acteur principal :', df_acteur['Acteur'])
+    score_acteur1 = df_acteur[df_acteur['Acteur'] == acteur1]['Score'].values[0]
+with col2:    
+    acteur2 = st.selectbox('Choisir l\'acteur secondaire :', df_acteur['Acteur'])
+    score_acteur2 = df_acteur[df_acteur['Acteur'] == acteur2]['Score'].values[0]
+    score_acteur = (score_acteur1*1.125)+(score_acteur2*0.75)
+with col3:    
+    real = st.selectbox('Choisir le réalisateur :', df_real['realisateur'])
+    score_real = df_real[df_real['realisateur'] == real]['score'].values[0]
 
-acteur2 = st.selectbox('Choisir l\'acteur secondaire :', df_acteur['Acteur'])
-score_acteur2 = df_acteur[df_acteur['Acteur'] == acteur2]['Score'].values[0]
+col1, col2, col3 = st.columns(3)
+with col1:
+    scenar = st.selectbox('Choisir le scénariste :', df_scenar['scenariste'])
+    score_scenar = df_scenar[df_scenar['scenariste'] == scenar]['score'].values[0]
+with col2:    
+    distrib = st.selectbox('Choisir le distributeur :', df_distrib['distributeur'])
+    score_distrib  = df_distrib[df_distrib['distributeur'] == distrib]['score'].values[0]
+with col3:    
+    date_sortie = st.date_input('Choisir la date de sortie', min_value=date(2000, 1, 1), max_value=date(2023, 12, 31))
 
-score_acteur = (score_acteur1*1.125)+(score_acteur2*0.75)
+col1, col2 = st.columns(2)
+with col1:
+    pays = st.selectbox('Sélectionnez le pays', ['USA', 'France', 'Autre'])
+with col2:     
+    genre = st.selectbox('Sélectionnez le genre', ['Comédie', 'Documentaire', 'Action'])
 
-real = st.selectbox('Choisir le réalisateur :', df_real['realisateur'])
-score_real = df_real[df_real['realisateur'] == real]['score'].values[0]
-
-scenar = st.selectbox('Choisir le scénariste :', df_scenar['scenariste'])
-score_scenar = df_scenar[df_scenar['scenariste'] == scenar]['score'].values[0]
-
-distrib = st.selectbox('Choisir le distributeur :', df_distrib['distributeur'])
-score_distrib  = df_distrib[df_distrib['distributeur'] == distrib]['score'].values[0]
-
-date_sortie = st.date_input('Choisir la date de sortie', min_value=date(2000, 1, 1), max_value=date(2023, 12, 31))
-
-pays = st.selectbox('Sélectionnez le pays', ['USA', 'France', 'Autre'])
-
-genre = st.selectbox('Sélectionnez le genre', ['Comédie', 'Documentaire', 'Action'])
-
-budget = st.slider('Sélectionnez le budget en millions d\'euros', 10, 200, step=10, value=50)
-
-duree = st.slider('Sélectionnez la duree', 40, 200, step=20, value=100)
+col1, col2 = st.columns(2)
+with col1:
+    budget = st.slider('Sélectionnez le budget en millions d\'euros', 10, 200, step=10, value=50)
+with col2:    
+    duree = st.slider('Sélectionnez la duree', 40, 200, step=20, value=100)
 
 df_predict = pd.DataFrame(columns = ['budget_euro', 'acteur', 'realisateur', 'scenariste', 'distributeur', 'duree', 'USA', 'France', 'Famille', 'Comédie musicale', 'Musical', 'Comédie dramatique', 'Action', 'Aventure', 'Historique', 'Biopic', 'Guerre', 'Drame', 'Documentaire', 'Fantastique', 'Espionnage', 'Animation', 'Romance', 'Comédie', 'Policier', 'Epouvante-horreur', 'Thriller', 'Science Fiction', 'cos_jour_mois', 'sin_jour_mois', 'cos_mois', 'sin_mois', 'cos_jour_semaine', 'sin_jour_semaine'])
 
