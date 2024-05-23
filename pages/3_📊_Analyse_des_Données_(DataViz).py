22a2a4aba3a5fc4eec225ba53829f12ea0fa42a1
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 18:01:19 +0200

    Update 3_📊_Analyse_des_Données_(DataViz).py

--- pages/3_📊_Analyse_des_Données_(DataViz).py
+++ pages/3_📊_Analyse_des_Données_(DataViz).py
@@ -13,7 +13,7 @@ from bokeh.plotting import figure
 from bokeh.models import HoverTool
 from include.css_and_credit import css_and_credit
 
-st.set_page_config(page_title="Exploitation cinématographique", layout="wide")
+st.set_page_config(page_title="Exploitation cinématographique", page_icon='🎬', layout="centered")
 
 css_and_credit()
 
