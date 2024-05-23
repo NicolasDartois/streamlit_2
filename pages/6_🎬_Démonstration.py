527d3e6fadb29f82e8aaec7de1dfc4daf9cfedb1
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-23 10:15:07 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import joblib as jl
 import pandas as pd
+from include.css_and_credit import css_and_credit
+
+css_and_credit()
+
+st.header("🎬Démonstration🎬")
 
 clf = jl.load("models/GB.joblib")
 df = pd.read_csv('data/score_acteur.csv')
