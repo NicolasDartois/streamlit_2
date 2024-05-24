419fa837d7c7746f54515865e4a6011da2cbe256
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-24 14:19:51 +0200

    Update 🎞️_Présentation_du_projet.py

--- 🎞️_Présentation_du_projet.py
+++ 🎞️_Présentation_du_projet.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 from include.css_and_credit import css_and_credit
 
 st.set_page_config(page_title="Exploitation cinématographique", page_icon='🎬', layout="wide")
-st.markdown("""<div class ="centered-content"><div class="box"><h1>🎥 L'IA au service de la production cinématographique ! 🎥</h1></div></div>""")
+st.markdown("""<div class ="centered-content"><h1>🎥 L'IA au service de la production cinématographique ! 🎥</h1></div>""", unsafe_allow_html=True)
 
 css_and_credit()
 
