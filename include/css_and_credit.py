ba9df1c3d3409092810b65f1b928f1fcb378659a
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-24 14:15:24 +0200

    Update css_and_credit.py

--- include/css_and_credit.py
+++ include/css_and_credit.py
@@ -17,7 +17,7 @@ def css_and_credit():
         display: flex;
         justify-content: center;
         align-items: center;
-        width: 80%;
+        width: 60%;
         min-width: 800px;
         margin-left: auto;
         margin-right: auto;
