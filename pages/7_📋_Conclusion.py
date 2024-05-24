664974f0200bfa0b98b6a396c2d40a51d71cceaf
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-24 22:22:16 +0200

    Update 7_📋_Conclusion.py

--- pages/7_📋_Conclusion.py
+++ pages/7_📋_Conclusion.py
@@ -15,4 +15,4 @@ st.markdown("""<div class ="centered-content"><div class="box"><h3>Notre jeu de 
     <p>• NL’ajout de données temporelles cohérentes par rapport à la date de sortie des films (évaluation de la notoriété des acteurs au moment de la sortie de chacun de ses films…) permettrait d’affiner notre scoring.</p>
     <p>• Les tendances culturelles, bien que difficiles à transformer en data, peuvent parfois influer sur un succès ou un échec.</p>
     <p>• L’actualisation des budgets en fonction de l’érosion monétaire permettrait d’obtenir des budgets plus précis.</p>
-    <p>NB: nous aurions pu avoir accès à des données plus riches, mais cela impliquait des abonnements payants, ce qui était contraire à l’esprit du projet.</p>
+    <p>NB: nous aurions pu avoir accès à des données plus riches, mais cela impliquait des abonnements payants, ce qui était contraire à l’esprit du projet.</p>""", unsafe_allow_html=True)
