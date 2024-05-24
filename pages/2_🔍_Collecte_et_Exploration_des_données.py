7625b87c1b626b7b125e80ad3341bba005e2e717
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-24 14:13:56 +0200

    Update 2_🔍_Collecte_et_Exploration_des_données.py

--- pages/2_🔍_Collecte_et_Exploration_des_données.py
+++ pages/2_🔍_Collecte_et_Exploration_des_données.py
@@ -6,10 +6,9 @@ st.set_page_config(page_title="Exploitation cinématographique", page_icon='🎬
 css_and_credit()
 st.header("🔍Collecte et Exploration des données🔍")
 
-
 col1, col2, col3 = st.columns([2,5,2])
 with col2:
-  st.markdown("""<div class="box centered-content"><h3>Notre jeu de donnée lors du démarrage et son évolution</h3><p>Au cours de l’analyse initiale de notre jeu de données et à la lumière de nos premiers acquis en matière de formation, 
+  st.markdown("""<div class ="centered-content"><div class="box"><h3>Notre jeu de donnée lors du démarrage et son évolution</h3><p>Au cours de l’analyse initiale de notre jeu de données et à la lumière de nos premiers acquis en matière de formation, 
     nous avons constaté que nos données étaient insuffisantes pour élaborer un modèle de machine learning robuste. Plusieurs défis se sont présentés : d’abord, notre jeu de données contenait un nombre excessif de valeurs manquantes. 
     De plus, nous hésitions encore sur la variable cible à prédire, hésitant entre les revenus générés et les votes des spectateurs.Par ailleurs, notre jeu de données couvrait le marché mondial, 
     ce qui nous a rapidement motivés à nous focaliser sur le marché français, nécessitant de trouver de nouvelles sources de données.</p>
@@ -24,4 +23,4 @@ with col2:
     <p>• Sur IMDB, nous avons récupéré un fichier global contenant les identifiants IMDB des films, leur durée et leurs titres-originaux (toujours dans l’optique de l’utiliser comme clé d’indexation).</p>
     <p>• Enfin, nous avons décidé de scraper sur IMDB et Allociné des données permettant de construire un score de notoriété pour chaque acteur, réalisateur ou scénariste, en nous basant sur le Starmeter, le nombre de récompenses reçues, le nombre de films réalisés et la durée de leur carrière.</p>
     <p>Ces démarches nous ont permis de bâtir un jeu de données plus complet et pertinent pour le développement de notre modèle prédictif du nombre-d’entrées sur le marché français.</p>
-    <img src="https://github.com/NicolasDartois/streamlit_2/blob/main/images/Schema_budget.png?raw=true"  class="fit-img"/></div>""", unsafe_allow_html=True)
+    <img src="https://github.com/NicolasDartois/streamlit_2/blob/main/images/Schema_budget.png?raw=true"  class="fit-img"/></div></div>""", unsafe_allow_html=True)
