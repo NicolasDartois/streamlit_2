cd55401ad8cf3897a6921c54b65be92b9d8e9739
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-26 11:35:37 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -129,9 +129,47 @@ if 'button2_clicked' not in st.session_state:
 if st.button('Scotty ?!'):
     st.session_state.button1_clicked = True
 
-if st.session_state.button1_clicked:
-    st.dataframe(df_predict[['budget_euro', 'acteur', 'realisateur', 'scenariste', 'distributeur', 'duree', 'USA', 'France', 'Action', 'Documentaire', 'Comédie', 'cos_jour_mois', 'sin_jour_mois', 'cos_mois', 'sin_mois', 'cos_jour_semaine', 'sin_jour_semaine']])
-    st.write(f'Le modèle prédit <span style="font-size:20px; color:#27AE60;"><b>{str(round(prediction[0]))}</b></span> entrées la première semaine en France.', unsafe_allow_html=True)
+if st.session_state.button1_clicked:data_predict = {
+    'budget_euro': int(budget*1000000),
+    'acteur': score_acteur,
+    'realisateur': score_real,
+    'scenariste': score_scenar,
+    'distributeur': score_distrib,
+    'duree': int(duree),
+    'USA': True if pays == 'USA' else False,
+    'France': True if pays == 'France' else False,
+    'Famille': False,
+    'Comédie musicale': False,
+    'Musical': False,
+    'Comédie dramatique': False,
+    'Action': True if genre == 'Action' else False,
+    'Aventure': False,
+    'Historique': False,
+    'Biopic': False,
+    'Guerre': False,
+    'Drame': False,
+    'Documentaire': True if genre == 'Documentaire' else False,
+    'Fantastique': False,
+    'Espionnage': False,
+    'Animation': False,
+    'Romance': False,
+    'Comédie': True if genre == 'Comédie' else False,
+    'Policier': False,
+    'Epouvante-horreur': False,
+    'Thriller': False,
+    'Science Fiction': False,
+    'cos_jour_mois': np.cos(date_sortie.day),
+    'sin_jour_mois': np.sin(date_sortie.day),
+    'cos_mois': np.cos(date_sortie.month),
+    'sin_mois': np.sin(date_sortie.month),
+    'cos_jour_semaine': np.cos(date_sortie.weekday()),
+    'sin_jour_semaine': np.sin(date_sortie.weekday())
+    }
+    df_predict.loc[0] = data_predict
+    input_data = df_predict
+    prediction = model.predict(input_data)
+    st.dataframe(df_predict[['budget_euro','acteur','realisateur','scenariste','distributeur','duree','USA','France','Action','Documentaire','Comédie','cos_jour_mois','sin_jour_mois','cos_mois','sin_mois','cos_jour_semaine','sin_jour_semaine']])
+    st.write(f'Le modèle predit <span style="font-size:20px; color:#27AE60;"><b>{str(round(prediction[0]))}</b></span> entrées la première semaine en france.', unsafe_allow_html=True)
     
     # Deuxième bouton
     if st.button('Mais que fait John Connor ?!'):
