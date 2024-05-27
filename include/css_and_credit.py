33e0c437eca63c6d059a7ddc8078dc8c4c0f2166
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-27 16:48:35 +0200

    Update css_and_credit.py

--- include/css_and_credit.py
+++ include/css_and_credit.py
@@ -15,39 +15,6 @@ def css_and_credit():
     
     background_image = '''
         <style>
-    .tabs {
-        margin: 0 auto;
-    }
-    
-    .tabs input[type="radio"] {
-        display: none;
-    }
-    
-    .tabs label {
-        padding: 10px 20px;
-        background: #eee;
-        cursor: pointer;
-        display: inline-block;
-    }
-    
-    .tabs .content > div {
-        display: none;
-        border-top: none;
-    }
-    
-    #tab1:checked ~ .content #content1,
-    #tab2:checked ~ .content #content2,
-    #tab3:checked ~ .content #content3 {
-        display: block;
-    }
-    
-    .tabs input[type="radio"]:checked + label {
-        background: #fff;
-    }
-    
-    .tabs label:not(:last-of-type) {
-        border-right: none;
-    }
     .centered-content {
         display: flex;
         justify-content: center;
