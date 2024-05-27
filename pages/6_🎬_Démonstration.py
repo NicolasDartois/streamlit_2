ae44acc6b1c97a62cac2135a6f748c0a0de4beb5
NicolasDartois <151928781+NicolasDartois@users.noreply.github.com>
2024-05-28 00:58:51 +0200

    Update 6_🎬_Démonstration.py

--- pages/6_🎬_Démonstration.py
+++ pages/6_🎬_Démonstration.py
@@ -39,6 +39,8 @@ df_scenar = pd.read_csv('data/score_scenar.csv')
 df_distrib = pd.read_csv('data/score_distrib.csv')
 
 col1, col2, col3, col4, col5, col6 = st.columns([1,2,2,2,2,1])
+with col1:
+    ia = st.checkbox('ia')
 with col2:
     acteur1 = st.selectbox('Acteur principal :', df_acteur['Acteur'])
     score_acteur1 = df_acteur[df_acteur['Acteur'] == acteur1]['Score'].values[0]
@@ -117,41 +119,41 @@ with col2:
                 prediction = model.predict(input_data)
                 st.dataframe(df_predict[['budget_euro','acteur','realisateur','scenariste','distributeur','duree','USA','France','Action','Documentaire','Comédie','cos_jour_mois','sin_jour_mois','cos_mois','sin_mois','cos_jour_semaine','sin_jour_semaine']])
                 st.write(f'Le modèle predit <span style="font-size:20px; color:#27AE60;"><b>{str(round(prediction[0]))}</b></span> entrées la première semaine en france.', unsafe_allow_html=True)
-                if
-                prompt_synopsis = f"""Génère un synopsis en français pour un film {pays} sorti en {date_sortie.year}, réalisé par {real}, distribué par {distrib}, dans le genre {genre}, avec {acteur1} en acteur principal et {acteur2} en acteur secondaire.(Attention, ne donne pas le titre dans ta reponse)"""
-                response_synopsis = client.chat.completions.create(
-                    model="gpt-4",
-                    messages=[{"role": "user", "content": prompt_synopsis}],
-                )
-        
-                prompt_titre = f"""Génère un titre en français pour ce synopsis (Attention, uniquement le titre dans ta reponse, rien d'autre) : {response_synopsis.choices[0].message.content}"""
-                response_titre = client.chat.completions.create(
-                    model="gpt-4",
-                    messages=[{"role": "user", "content": prompt_titre}],
-                )
-                
-                prompt_affiche = f"""Génère une affiche en français pour ce film {response_titre.choices[0].message.content} qui a ce synopsis (Aucun acteur sur l'affiche ne doit ressembler à une personne réelle) : {response_synopsis.choices[0].message.content}"""
-                response_affiche = client.images.generate(
-                    model="dall-e-3", 
-                    prompt=prompt_affiche, 
-                    n=1, 
-                    size="1024x1792"
-                )
-              
-                col1, col2, col3, col4 = st.columns([2, 3, 13, 2])
-                st.markdown(f"""
-                    <div class="wrapper2">
-                        <div class="box">
-                            <img src={response_affiche.data[0].url} class="fit-img"/>
-                        </div>
-                        <div class="box">
-                            <h3>{response_titre.choices[0].message.content}</h3>
-                            <h6>Le <b>{date_sortie}</b> en salle</h6>
-                            <h6>Par <b>{real}</b></h6>
-                            <h6> Avec <b>{acteur1}</b> et <b>{acteur2}</b></h6>
-                            <p>{response_synopsis.choices[0].message.content}</p>
-                        </div>
-                    </div>""", unsafe_allow_html=True)
+                if ia:
+                    prompt_synopsis = f"""Génère un synopsis en français pour un film {pays} sorti en {date_sortie.year}, réalisé par {real}, distribué par {distrib}, dans le genre {genre}, avec {acteur1} en acteur principal et {acteur2} en acteur secondaire.(Attention, ne donne pas le titre dans ta reponse)"""
+                    response_synopsis = client.chat.completions.create(
+                        model="gpt-4",
+                        messages=[{"role": "user", "content": prompt_synopsis}],
+                    )
+            
+                    prompt_titre = f"""Génère un titre en français pour ce synopsis (Attention, uniquement le titre dans ta reponse, rien d'autre) : {response_synopsis.choices[0].message.content}"""
+                    response_titre = client.chat.completions.create(
+                        model="gpt-4",
+                        messages=[{"role": "user", "content": prompt_titre}],
+                    )
+                    
+                    prompt_affiche = f"""Génère une affiche en français pour ce film {response_titre.choices[0].message.content} qui a ce synopsis (Aucun acteur sur l'affiche ne doit ressembler à une personne réelle) : {response_synopsis.choices[0].message.content}"""
+                    response_affiche = client.images.generate(
+                        model="dall-e-3", 
+                        prompt=prompt_affiche, 
+                        n=1, 
+                        size="1024x1792"
+                    )
+                  
+                    col1, col2, col3, col4 = st.columns([2, 3, 13, 2])
+                    st.markdown(f"""
+                        <div class="wrapper2">
+                            <div class="box">
+                                <img src={response_affiche.data[0].url} class="fit-img"/>
+                            </div>
+                            <div class="box">
+                                <h3>{response_titre.choices[0].message.content}</h3>
+                                <h6>Le <b>{date_sortie}</b> en salle</h6>
+                                <h6>Par <b>{real}</b></h6>
+                                <h6> Avec <b>{acteur1}</b> et <b>{acteur2}</b></h6>
+                                <p>{response_synopsis.choices[0].message.content}</p>
+                            </div>
+                        </div>""", unsafe_allow_html=True)
 
                 
                 
